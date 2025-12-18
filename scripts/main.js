e5d0955907a83da88cbfc9f03abdc031436ddf01
mahubanemathume-del <mahubanemathume@gmail.com>
2025-12-18 02:35:48 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,13 @@ let index = 0;
 let imgElement = document.querySelector(".hero-img");
 
 function showImage() {
-  imgElement.style.opacity = 0; // fade out current
+  imgElement.style.opacity = 0;
   setTimeout(() => {
     index = (index + 1) % images.length;
-    imgElement.src = images[index]; // swap image
-    imgElement.style.opacity = 1;   // fade in new
-  }, 500); // overlap timing
+    imgElement.src = images[index];
+    imgElement.style.opacity = 1;
+  }, 500); //
 }
 
 setInterval(showImage, 5000);
+
